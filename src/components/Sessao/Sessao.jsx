ffe550c1a3bf7c77dc78fd22342cea7f56f520ca
Lucas Tymoschenko <lucastymos@gmail.com>
2023-03-22 15:27:07 -0300

    teste img

--- src/components/Sessao/Sessao.jsx
+++ src/components/Sessao/Sessao.jsx
@@ -4,7 +4,7 @@ function Sessao() {
     return ( 
         <div className="Sessao">
             <h2>O que oferecemos?</h2>
-            <img src=".\src\components\Sessao\img\caminhando.png" alt="" />
+            <img src="src/assets/img/caminhando.png" alt="" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore fugiat sunt corporis sequi dolorem odio ducimus magni cum quibusdam rem aliquam, expedita nam quasi maiores omnis doloremque saepe unde suscipit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, rerum. Voluptas accusantium commodi, corrupti esse officia id animi.</p>
         </div>
      );
